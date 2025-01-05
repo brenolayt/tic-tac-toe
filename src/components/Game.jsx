3262f3fe0ba1f4c249134550ed039c2eda7e27d5
Breno Laytynher <brenolaytynher@outlook.com>
2025-01-05 19:08:38 -0300

    check win function implemented

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -8,20 +8,51 @@ export default function HomeScreen() {
         "", "", "",
         "", "", ""
     ]);
+    const [winner, setWinner] = useState(null);
     const [count, setCount] = useState(1);
     const [currentPlayer, setCurrentP] = useState("X");
 
+    function checkWin(arr) {
+        const winningCombinations = [
+          [0, 1, 2],
+          [3, 4, 5],
+          [6, 7, 8], 
+          [0, 3, 6], 
+          [1, 4, 7],
+          [2, 5, 8],
+          [0, 4, 8],
+          [2, 4, 6],
+        ];
+      
+        for (const combination of winningCombinations) {
+          const [a, b, c] = combination;
+          if (arr[a] !== "" && arr[a] === arr[b] && arr[a] === arr[c]) {
+              return arr[a];
+          }
+        }
+        return null;
+      }
 
     function handleClick(ind) {
-        if (gameArr[ind] !== "") return;
+        if (gameArr[ind] !== "" || winner) return;
         const newGameArr = [...gameArr];
         newGameArr[ind] = currentPlayer;
         
-        setCurrentP(currentPlayer == "X" ? "O" : "X");
         setGA(newGameArr);
         setCount(prevValue => prevValue + 1);
 
-        if (count == 9) alert("The End!");
+        const gameWinner = checkWin(newGameArr);
+
+        if (gameWinner) {
+            setWinner(gameWinner);
+            setTimeout(() => {
+                alert(`${gameWinner} won!`);
+            }, 100);
+        }
+
+        if (count == 9) alert("Draw!");
+
+        setCurrentP(currentPlayer == "X" ? "O" : "X");
     }
 
     return (
