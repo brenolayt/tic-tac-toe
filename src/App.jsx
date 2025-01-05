914e79a34d06514072082731c9de2184b79a5d4e
Breno Laytynher <brenolaytynher@outlook.com>
2025-01-04 22:10:04 -0300

    second commit🤯

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <main className="h-screen w-full bg-slate-900 flex justify-center items-center">
+      <div className="h-4/5 w-3/5 bg-white">
+        
+      </div>
+    </main>
+  );
 }
 
 export default App
