35d2df4c9c5bc92b738e8a1b14bbbf580f08e814
Breno Laytynher <brenolaytynher@outlook.com>
2025-01-05 18:41:37 -0300

    starting game logic🧐

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -3,13 +3,31 @@ import GameTile from "./GameTile";
 
 
 export default function HomeScreen() {
-    const [gameArr, setGA] = useState(["1","","","","","","","",""]);
+    const [gameArr, setGA] = useState([
+        "", "", "",
+        "", "", "",
+        "", "", ""
+    ]);
+    const [count, setCount] = useState(1);
+    const [currentPlayer, setCurrentP] = useState("X");
 
+
+    function handleClick(ind) {
+        if (gameArr[ind] !== "") return;
+        const newGameArr = [...gameArr];
+        newGameArr[ind] = currentPlayer;
+        
+        setCurrentP(currentPlayer == "X" ? "O" : "X");
+        setGA(newGameArr);
+        setCount(prevValue => prevValue + 1);
+
+        if (count == 9) alert("The End!");
+    }
 
     return (
-        <div className="h-3/4 w-3/6 bg-slate-50 rounded-xl grid grid-cols-3">
-            {gameArr.map(el => {
-                return <GameTile>{el}</GameTile>
+        <div className="h-3/4 w-3/6 bg-slate-50 rounded-xl grid grid-cols-3 grid-rows-3">
+            {gameArr.map((el, index) => {
+                return <GameTile key={index} onClick={() => handleClick(index)}>{el}</GameTile>
             })}
         </div>
     );
--- src/components/GameTile.jsx
+++ src/components/GameTile.jsx
@@ -1,6 +1,8 @@
-export default function GameTile({children}) {
-    return (<div className="w-auto h-auto border-4 border-gray-900 grid place-content-center font-sans font-bold text-5xl hover:bg-gray-500
-    hover:text-gray-50 transition-all">
+export default function GameTile({ children, ...props }) {
+    return (
+      <div className="w-full h-full grid place-items-center border-4 border-gray-900 box-border
+      font-sans font-bold text-7xl hover:bg-gray-500 hover:text-gray-50 transition-all" {...props}>
         {children}
-    </div>);
-}
+      </div>
+    );
+  }
